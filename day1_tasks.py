3f522f624d6fc14037cbabfedb98cb8d8484c68e
Hifzan-uddin <hifzanuddin9@gmail.com>
2025-10-04 03:54:31 +0500

    a little bit update in my code

--- day1_tasks.py
+++ day1_tasks.py
@@ -1,25 +1,25 @@
-#Print your name, age, and favorite hobby using variables.
-# name="hifzan"
-# age=21
-# hobby="cricket"
-# print("my name is ",name,"i am ",age ,"year old","and my favrite hobby is ",)
+# Print your name, age, and favorite hobby using variables.
+name="hifzan"
+age=21
+hobby="cricket"
+print("my name is ",name,"i am ",age ,"year old","and my favrite hobby is ",)
 
 
-#     probllem 2
-#Ask user’s name and print a greeting.
-# name = str(input("enter your name bro/sis/darling/wify :"))
-# print("hello,",name ,"sweety !")
+   
+# Ask user’s name and print a greeting.
+name = str(input("enter your name bro/sis/darling/wify :"))
+print("hello,",name ,"sweety !")
 
 
-#   problem 
-#Ask for two numbers and print their sum.
-# num1=int(input("enter first number :"))
-# num2=int(input("enetr second number :"))
-# sum=num1+num2
-# print("the sum of two number is :",sum)
+
+# Ask for two numbers and print their sum.
+num1=int(input("enter first number :"))
+num2=int(input("enetr second number :"))
+sum=num1+num2
+print("the sum of two number is :",sum)
 
 
-#Ask user’s age and show their age next year.
-# age =int (input("enter your age :"))
-# next_year=age+1
-# print("your age next year will be :",next_year)
+# Ask user’s age and show their age next year.
+age =int (input("enter your age :"))
+next_year=age+1
+print("your age next year will be :",next_year)
